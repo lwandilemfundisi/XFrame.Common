0c680c0d1843d2cef3db24cd9ddc46c8b08f99a4
Lwandile Mfundisi <Lwandile.Mfundisi@media24.com>
2023-07-31 18:41:55 +0200

    added common optimistic exception

---
+++ Exceptions/OptimisticConcurrencyException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace XFrame.Common.Exceptions
+{
+    public class OptimisticConcurrencyException : Exception
+    {
+        public OptimisticConcurrencyException(string message)
+            : base(message)
+        {
+        }
+
+        public OptimisticConcurrencyException(
+            string message,
+            Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
