18aed3d4aab6ac74aa0abb92ac0d8e90fdf4368b
Lwandile Mfundisi <Lwandile.Mfundisi@media24.com>
2023-07-31 19:41:41 +0200

    save

---
+++ Exceptions/InvariantException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace XFrame.Common.Exceptions
+{
+   public class InvariantException : Exception
+   {
+      public InvariantException() { }
+
+      public InvariantException(string message) : base(message) { }
+
+      public InvariantException(string message, Exception inner) : base(message, inner) { }
+
+      protected InvariantException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+   }
+}
---
+++ Invariant.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using XFrame.Common.Exceptions;
+using XFrame.Common.Extensions;
+
+namespace XFrame.Common
+{
+    public static class Invariant
+    {
+        public static void ArgumentNotNull(object argument, Func<string> argumentName)
+        {
+            if (argument == null)
+            {
+                throw new InvariantException(argumentName());
+            }
+        }
+
+        public static void ArgumentNotEmpty(string argument, Func<string> argumentName)
+        {
+            if (argument.IsNullOrEmpty())
+            {
+                throw new InvariantException("Empty argument: " + argumentName());
+            }
+        }
+
+        public static void IsNotNull(object value, Func<string> error)
+        {
+            if (value.IsNull())
+            {
+                throw new InvariantException(error());
+            }
+        }
+
+        public static void IsNull(object value, Func<string> error)
+        {
+            if (value.IsNotNull())
+            {
+                throw new InvariantException(error());
+            }
+        }
+
+        public static void IsTrue(bool test, Func<string> error)
+        {
+            if (!test)
+            {
+                throw new InvariantException(error());
+            }
+        }
+
+        public static void IsFalse(bool test, Func<string> error)
+        {
+            if (test)
+            {
+                throw new InvariantException(error());
+            }
+        }
+
+        public static void Fail(string error)
+        {
+            throw new InvariantException(error);
+        }
+    }
+}
